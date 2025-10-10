2ab4def3aaa6af5928e04079fa662a481dcb0c26
DyerMandujano <dyer0206@gmail.com>
2025-10-10 17:18:29 -0500

    Primer commit - Proyecto Spring Boot Api-Skillup

---
+++ src/main/java/com/skillup/api/rest/ApiSkillupApplication.java
@@ -0,0 +1,13 @@
+package com.skillup.api.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSkillupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSkillupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skillup/api/rest/ApiSkillupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skillup.api.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSkillupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
